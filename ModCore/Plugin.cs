0fc61be23bcf17ac4349f14851bd77374b7b339f
Pikachu <1223188723@qq.com>
2026-04-20 03:54:48 +0800

    Minimal-CSFF

--- ModCore/Plugin.cs
+++ ModCore/Plugin.cs
@@ -5,7 +5,7 @@ namespace ModCore;
 [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
 internal class Plugin : BaseUnityPlugin<Plugin>
 {
-    public const string PluginGuid = "Pikachu.CSTI.ModCore";
+    public const string PluginGuid = "Pikachu.CSFF.ModCore";
     public const string PluginName = "ModCore";
     public const string PluginVersion = "0.0.0.0";
 
